fbe465f0a5bdddc6998c77e344fcfb3f61004f21
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-08-30 18:19:47 -0500

    Generando la capa DAO - generando la lógica de negocio

---
+++ src/main/java/com/vaescode/contactos/dao/AgendaDaoImpl.java
@@ -0,0 +1,57 @@
+package com.vaescode.contactos.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.vaescode.contactos.model.Contacto;
+
+@Repository
+public class AgendaDaoImpl implements AgendaDao {
+
+	@Autowired
+	AgendaJpaSpring agenda;
+
+	@Override
+	public void agregarContacto(Contacto contacto) {
+		agenda.save(contacto);
+	}
+
+	@Override
+	public Contacto recuperarContacto(String email) {
+		// TODO Auto-generated method stub
+		return agenda.findByEmail(email);
+	}
+
+	@Override
+	public void eliminarContacto(String email) {
+		// TODO Auto-generated method stub
+		agenda.eliminarPorEmail(email);
+	}
+
+	@Override
+	public List<Contacto> devolverContactos() {
+		// TODO Auto-generated method stub
+		return agenda.findAll();
+	}
+
+	@Override
+	public void eliminarContacto(int idContacto) {
+		// TODO Auto-generated method stub
+		agenda.deleteById(idContacto);
+	}
+
+	@Override
+	public Contacto recuperarContacto(int idContacto) {
+		// TODO Auto-generated method stub
+		return agenda.findById(idContacto).orElse(null);
+	}
+
+	@Override
+	public void actualizarContacto(Contacto contacto) {
+		// TODO Auto-generated method stub
+			agenda.save(contacto);
+	}
+
+}
