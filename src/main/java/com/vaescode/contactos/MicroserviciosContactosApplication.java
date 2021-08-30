932967b93d12f0431a7918ed66ac421dc5a00c41
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-08-30 17:04:57 -0500

    Inicializando microservicios de contactos

---
+++ src/main/java/com/vaescode/contactos/MicroserviciosContactosApplication.java
@@ -0,0 +1,13 @@
+package com.vaescode.contactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosContactosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosContactosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaescode/contactos/MicroserviciosContactosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaescode.contactos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosContactosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
