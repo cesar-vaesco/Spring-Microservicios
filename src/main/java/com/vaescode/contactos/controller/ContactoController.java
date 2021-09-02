ed5b4733fdf17e865829db16319d51ff32af4a17
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-09-02 18:45:37 -0500

    Preparando aplicación para generar archivo .jar

--- src/main/java/com/vaescode/contactos/controller/ContactoController.java
+++ src/main/java/com/vaescode/contactos/controller/ContactoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.vaescode.contactos.model.Contacto;
 import com.vaescode.contactos.service.AgendaService;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class ContactoController {
 
@@ -43,7 +45,7 @@ public class ContactoController {
 		service.actualizarContacto(contacto);
 	}
 	
-	@DeleteMapping(value="eliminar/{id}")
+	@DeleteMapping(value="contactos/{id}")
 	public void eliminarPorId(@PathVariable("id") int id) {
 		service.eliminarContacto(id);
 	}
